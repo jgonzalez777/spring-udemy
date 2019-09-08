b24dda75dbb70432aecb5b094de1a55893e0a349
jgonzalez777 <jorge.hermon6@gmail.com>
2019-09-08 10:11:49 -0500

    Primeras anotaciones conocidas en Spring @Controller @RequestMapping @GetMapping

--- Udemy/src/main/java/com/desarrollo/udemy/controllers/IndexController.java
+++ Udemy/src/main/java/com/desarrollo/udemy/controllers/IndexController.java
@@ -5,13 +5,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/app")
+@Controller //Anotación para declarar que esta clase es controladora en Spring Framework
+@RequestMapping("/app") //Anotación para declarar que la cadena "/app" será la previa para cada método declarado
 public class IndexController {
 	
-	@GetMapping({"/", "/home", "/index", "" })
+	@GetMapping({"/", "/home", "/index", "" })//Un método GET asociado a 4 nombres posibles de URL
 	public String index(Model model) {
 		model.addAttribute("titulo", "Hola mundo desde Spring");
-		return "index";
+		return "index";//Nombre que recibe la vista asociada al método index
 	}
 }
