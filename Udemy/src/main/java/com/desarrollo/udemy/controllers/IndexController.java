ceefaae465eaf2daaa3b6466d8803f5f43f778ba
Jorge Gonzalez <jgonzalez777@gmail.com>
2019-09-11 08:03:58 -0500

    Model - Se crea el Modelo Usuario (POJO), se crea su metodo handler perfil, se inicializa un objeto de tipo usuario y finalmente se muestra por pantalla con Thymeleaf

--- Udemy/src/main/java/com/desarrollo/udemy/controllers/IndexController.java
+++ Udemy/src/main/java/com/desarrollo/udemy/controllers/IndexController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.desarrollo.udemy.models.Usuario;
+
 @Controller //Anotación para declarar que esta clase es controladora en Spring Framework
 @RequestMapping("/app") //Anotación para declarar que la cadena "/app" será la previa para cada método declarado
 public class IndexController {
@@ -13,5 +15,15 @@ public class IndexController {
 	public String index(Model model) {
 		model.addAttribute("titulo", "Hola mundo desde Spring");
 		return "index";//Nombre que recibe la vista asociada al método index
+	}
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Jorge");
+		usuario.setApellido("Gonzalez");//En la práctica este dato viene de la DB o de un servicio
+		model.addAttribute("usuario", usuario);//Se pasa al Model el objeto usuario
+		model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));//Se pasa al Model el string titulo
+		return "perfil"; //Se crea la vista perfil.html
 	}
 }
---
+++ Udemy/src/main/java/com/desarrollo/udemy/models/Usuario.java
@@ -0,0 +1,30 @@
+package com.desarrollo.udemy.models;
+
+/*
+ * Creación de un objeto Java simple
+ * */
+public class Usuario {
+	
+	private String nombre;
+	
+	private String apellido;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
+	
+
+}
